0eabf9e8f4a73bb2c9c62c24a61c617ac9cac356
BB Caspian <casfian@gmail.com>
2025-01-26 11:22:46 +0800

    Add files via upload

---
+++ pages/1_profile.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("# Profile")
+st.write("""
+This is the Profile page
+""")
---
+++ pages/2_about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("# About")
+st.write("""
+This is the About page
+""")
